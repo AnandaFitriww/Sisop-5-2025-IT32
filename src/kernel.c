ca74f7fcc5357a59e30041a549745633f04a910d
Rend00T <ghazali.raihan789@gmail.com>
2025-06-03 23:15:23 +0700

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -8,15 +8,57 @@ int main() {
 
 void printString(char *str)
 {
-  //TODO: Implementasi fungsi untuk mencetak string
+  while (*str != 0) {
+    __asm__ volatile(
+      "mov $0x0E, %%ah \n"  
+      "mov %0, %%al \n"
+      "int $0x10"
+      :
+      : "r" (*str)
+    );
+    str++;
+  }
 }
 
 void readString(char *buf)
 {
-  //TODO: Implementasi fungsi untuk membaca string
+  int i = 0;
+  char c = 0;
+
+  while (1) {
+    __asm__ volatile(
+      "mov $0x00, %%ah \n"  
+      "int $0x16 \n"
+      "mov %%al, %0"
+      : "=r" (c)
+      :
+      : "ah", "al"
+    );
+
+    if (c == 13 || c == 10) {
+      break;
+    } else if (c == 8) {
+      if (i > 0) {
+        i--;
+        printString("\b \b");
+      }
+    } else {
+      buf[i++] = c;
+      __asm__ volatile(
+        "mov $0x0E, %%ah \n"
+        "mov %0, %%al \n"
+        "int $0x10"
+        :
+        : "r" (c)
+      );
+    }
+  }
+  buf[i] = '\0'; 
+  printString("\n");
 }
 
 void clearScreen()
 {
-  //TODO: Implementasi fungsi untuk membersihkan layar
+
+printString("\033[2J\033[H");
 }
