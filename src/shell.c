da780216754ac81fcb8e3793ed62480046cb3cb7
Rend00T <ghazali.raihan789@gmail.com>
2025-06-03 23:31:55 +0700

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -4,16 +4,37 @@
 
 void shell() {
   char buf[128];
+  char username[64] = "user";
+  char command[16];
+  char args[2][64];
+
   printString("Welcome to EorzeOS!\n");
+
   while (true) {
+    printString(username);
     printString("> ");
     readString(buf);
 
-    if (strcmp(buf, "yo")) {
+    parseCommand(buf, command, args);
+
+    if (strcmp(command, "user")) {
+      if (args[0][0] == '\0') {
+        strcpy(username, "user");
+        printString("Username changed to user\n");
+      } else {
+        strcpy(username, args[0]);
+        printString("Username changed to ");
+        printString(username);
+        printString("\n");
+      }
+    }
+    else if (strcmp(command, "yo")) {
       printString("gurt\n");
-    } else if (strcmp(buf, "gurt")) {
+    }
+    else if (strcmp(command, "gurt")) {
       printString("yo\n");
-    } else {
+    }
+    else {
       printString(buf);
       printString("\n");
     }
@@ -21,7 +42,28 @@ void shell() {
 }
 
 void parseCommand(char *buf, char *cmd, char arg[2][64]) {
-  //TODO: Implementasi fungsi untuk mem-parsing perintah
-}
+  int i = 0, j = 0, k = 0;
+  clear(arg[0], 64);
+  clear(arg[1], 64);
 
-// Tambahkan fungsi bila perlu
+  while (buf[i] != ' ' && buf[i] != '\0') {
+    cmd[i] = buf[i];
+    i++;
+  }
+  cmd[i++] = '\0';
+
+  if (buf[i] != '\0') {
+    while (buf[i] != ' ' && buf[i] != '\0') {
+      arg[0][j++] = buf[i++];
+    }
+    arg[0][j] = '\0';
+  }
+
+  if (buf[i] != '\0') {
+    i++;
+    while (buf[i] != ' ' && buf[i] != '\0') {
+      arg[1][k++] = buf[i++];
+    }
+    arg[1][k] = '\0';
+  }
+}
